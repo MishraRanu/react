18dbce86b3d74fb9e8e02a024ee9fd6bfc6b5385
mrcoder991 <udaygirhepunje41@gmail.com>
2022-09-20 16:17:41 +0530

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
 import './App.css';
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-
-
 import About from './components/About';
+
 function App() {
   return (
     <>
-   <Route exact path="/home" element={ <Home/>}/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
